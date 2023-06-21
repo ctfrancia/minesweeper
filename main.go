3b48d3ee4f32cc7e8f1b511b19d41bb32776ba84
Christian Francia <christian.francia@adamo.es>
2023-06-21 18:41:56 +0200

    working without array boundary tests

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	c := 8  // columns
 	r := 8  // rows
-	nb := 5 // # bombs
+	nb := 1 // # bombs
 	start(c, r, nb)
 }
 
@@ -56,37 +56,89 @@ func setNumbers(bwb, bombLocs [][]int) [][]int {
 		fmt.Println(i, v)
 		row := v[0]
 		col := v[1]
-		fmt.Println("row-1", row-1)
+		// rowLength := len(numBoard[row])
+		// colLength := len(numBoard[col])
+		// fmt.Println("row-1", row-1, row-1 >= 0)
+		// mt.Println("col-1", col-1)
+		numBoard[row-1][col]++
+		numBoard[row+1][col]++
+		numBoard[row][col-1]++
+		numBoard[row][col+1]++
+		numBoard[row+1][col-1]++
+		numBoard[row-1][col+1]++
+		numBoard[row-1][col-1]++
+		numBoard[row+1][col+1]++
 
-		if row-1 <= 0 && col-1 <= 0 {
-			if numBoard[row-1][col] != -1 {
-				numBoard[row-1][col]++
+		/*
+			if row+1 >= rowLength {
+				if numBoard[row+1][col] != -1 {
+					numBoard[row+1][col]++
+				}
 			}
-		}
 
-		if row+1 > len(numBoard[row]) && col+1 > len(numBoard[col]) {
-			if numBoard[row+1][col] != -1 {
-				numBoard[row+1][col]++
+			if row-1 >= 0 {
+				if numBoard[row-1][col] != -1 {
+					numBoard[row-1][col]++
+				}
 			}
-		}
 
-		if col-1 <= 0 {
-			if numBoard[row][col-1] != -1 {
-				numBoard[row][col-1]++
+			if col-1 >= colLength {
+				if numBoard[row][col+1] != -1 {
+					numBoard[row][col+1]++
+				}
 			}
-		}
+			if col-1 >= 0 {
+				if numBoard[row][col-1] != -1 {
+					numBoard[row][col-1]++
+				}
+			}
+		*/
+		/*
+			if numBoard[row+1][col-1] != -1 {
+				numBoard[row+1][col-1]++
+			}
+			if numBoard[row-1][col+1] != -1 {
+				numBoard[row-1][col+1]++
+			}
+		*/
+		/*
+			if row-1 >= 0 {
+				if col-1 >= 0 {
 
-		if col+1 <= len(numBoard[col]) {
-			if numBoard[row][col+1] != -1 {
-				numBoard[row][col+1]++
+				}
 			}
-		}
+		*/
+
+		/*
+			if row-1 <= 0 && col-1 <= 0 {
+				if numBoard[row-1][col] != -1 {
+					numBoard[row-1][col]++
+				}
+			}
+
+			if row+1 > len(numBoard[row]) && col+1 > len(numBoard[col]) {
+				if numBoard[row+1][col] != -1 {
+					numBoard[row+1][col]++
+				}
+			}
+
+			if col-1 <= 0 {
+				if numBoard[row][col-1] != -1 {
+					numBoard[row][col-1]++
+				}
+			}
+
+			if col+1 <= len(numBoard[col]) {
+				if numBoard[row][col+1] != -1 {
+					numBoard[row][col+1]++
+				}
+			}
+		*/
 	}
 
 	return numBoard
 }
 
 func tileInBounds(len, place int) bool {
-
 	return true
 }
